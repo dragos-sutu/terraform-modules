740c7644dfeb31b0bbac9926bc4cd6bc72996327
Dragos Sutu <dragos.sutu@gmail.com>
2020-04-22 12:43:51 +0100

    Reference OUs by name in accounts, rename some resources.

--- aws/organizations/main.tf
+++ aws/organizations/main.tf
@@ -1,19 +1,25 @@
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
+locals {
+  # change var.organizational_units type to map,
+  # so that we can reference aws_organizations_organizational_unit
+  # by name (map key) in aws_organizations_account blocks
+  organizational_units = zipmap(var.organizational_units, var.organizational_units)
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_organizations_organization" "organization" {
+resource "aws_organizations_organization" "org" {
   feature_set = "ALL"
 }
 
-resource "aws_organizations_account" "child_account" {
+resource "aws_organizations_account" "account" {
   count = length(var.accounts)
 
-  email = var.accounts[count.index].email
-  name  = var.accounts[count.index].name
+  email     = var.accounts[count.index].email
+  name      = var.accounts[count.index].name
+  parent_id = aws_organizations_organizational_unit.ou[var.accounts[count.index].organizational_unit].id
+}
+
+resource "aws_organizations_organizational_unit" "ou" {
+  for_each = local.organizational_units
+
+  name      = each.key
+  parent_id = aws_organizations_organization.org.roots.0.id
 }
--- aws/organizations/vars.tf
+++ aws/organizations/vars.tf
@@ -1,11 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for provider"
+}
+
 variable "accounts" {
-  description = "list of accounts names and emails"
-  type = list(object({
-    email = string
-    name = string
+  description = "list of account objects, each containing account email, name and the organizational unit name that the account belongs to"
+  type        = list(object({
+    email               = string
+    name                = string
+    organizational_unit = string
   }))
 }
 
-variable "aws_region" {
-  description = "AWS region for provider"
+variable "organizational_units" {
+  description = "list of organizational units names"
+  type        = list(string)
 }

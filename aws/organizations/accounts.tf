78fd4b90adacf5e6232662fdc7387074f2b9992f
Dragos Sutu <dragos.sutu@gmail.com>
2020-04-24 12:20:43 +0100

    Rename oranizations.

--- aws/organizations/accounts.tf
+++ aws/organizations/accounts.tf
@@ -1,11 +1,11 @@
 resource "aws_organizations_account" "identity" {
   email     = var.account_identity.email
   name      = var.account_identity.name
-  parent_id = aws_organizations_organizational_unit.identity.id
+  parent_id = aws_organizations_organizational_unit.sre.id
 }
 
 resource "aws_organizations_account" "dev" {
   email     = var.account_dev.email
   name      = var.account_dev.name
-  parent_id = aws_organizations_organizational_unit.dev.id
+  parent_id = aws_organizations_organizational_unit.developers.id
 }

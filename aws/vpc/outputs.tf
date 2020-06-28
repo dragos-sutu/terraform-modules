0931a4e29dd9e830e9859393291c6b41479c188e
Dragos Sutu <dragos.sutu@gmail.com>
2020-06-28 14:54:04 +0100

    Add vpc and subnets ids to outputs, remove cidr info outputs

--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -1,7 +1,11 @@
-output "subnets_private" {
-  value = local.subnets_private
+output "id" {
+  value = aws_vpc.vpc.id
 }
 
-output "subnets_public" {
-  value = local.subnets_public
+output "subnets_private_ids" {
+  value = values(aws_subnet.private)[*].id
+}
+
+output "subnets_public_ids" {
+  value = values(aws_subnet.public)[*].id
 }

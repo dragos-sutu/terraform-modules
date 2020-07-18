1e9c4f1d80892be2b14565a6a43c1d0baf149558
Dragos Sutu <dragos.sutu@gmail.com>
2020-07-18 15:30:45 +0100

    Add zone id as output to route53-zone module

--- aws/route53-zone/outputs.tf
+++ aws/route53-zone/outputs.tf
@@ -2,6 +2,10 @@ output "name_servers" {
   value = aws_route53_zone.zone.name_servers
 }
 
+output "zone_id" {
+  value = aws_route53_zone.zone.zone_id
+}
+
 output "zone_name" {
   value = aws_route53_zone.zone.name
 }
